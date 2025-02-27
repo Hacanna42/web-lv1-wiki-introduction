8d7a857596d0716969c8436ca24cc41be6e35ebd
Hacanna42 <dtmdgus901@gmail.com>
2025-02-27 11:44:16 +0900

    feat: 크루위키 기본 구현 완료

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.addEventListener("DOMContentLoaded", function () {
+  // 부드러운 스크롤 기능
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 50,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
 
-console.log("알맞은 스크립트를 작성하세요");
+  // 댓글 작성 기능
+  document.getElementById("submit-comment").addEventListener("click", function () {
+    const commentInput = document.getElementById("comment-input");
+    const commentText = commentInput.value.trim();
+
+    if (commentText) {
+      const commentList = document.querySelector(".comment-list");
+      const newComment = document.createElement("li");
+      newComment.innerHTML = `
+        <div class="comment-item">
+          <div class="comment-author">
+            <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+            <span>방문자</span>
+          </div>
+          <div class="comment-content">${commentText}</div>
+        </div>
+      `;
+      commentList.appendChild(newComment);
+      commentInput.value = ""; // 입력 필드 초기화
+      alert("댓글이 등록되었습니다.");
+    } else {
+      alert("댓글을 입력해주세요!");
+    }
+  });
+});
